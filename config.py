6ea9787cf71b658eff7cb24a582cbe718f97a1fc
unknown <s.ventura6@campus.unimib.it>
2020-05-01 11:06:04 +0200

    add mean performance for fold

---
+++ config.py
@@ -0,0 +1,13 @@
+CONFIG =  {
+    'unimib': {
+        'log_file': 'data/datasets/UNIMIBDataset/record/'
+    },
+
+    'sbhar': {
+        'log_file': 'data/datasets/SBHAR_processed/record/'
+    },
+
+    'realdisp': {
+        'log_file' : 'data/datasets/REALDISP_processed/record/'
+    }
+}
