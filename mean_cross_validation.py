f74c20013a7b9c83d61b73a059ad5925be56c697
unknown <s.ventura6@campus.unimib.it>
2020-05-07 10:25:49 +0200

    fix minor

--- mean_cross_validation.py
+++ mean_cross_validation.py
@@ -2,14 +2,22 @@ import numpy as np
 from util.utils import mean_cross_performance
 from config import CONFIG
 
-for dataset in ['unimib', 'sbhar', 'realdisp']:
+for dataset in ['unimib']:
 
     path_log_file = './' + CONFIG[dataset]['log_file']
-    path_to_save = path_log_file + 'summary_mean.txt'
+    path_to_save_train = path_log_file + 'summary_mean_train.txt'
+    path_to_save_pre_train = path_log_file + 'summary_mean_pre_train.txt'
     
-    with open(path_to_save, 'w+') as f:
+    with open(path_to_save_train, 'w+') as f:
         for fold in range(10):
             history = np.loadtxt(fname=path_log_file+'log_history_train_{}.txt'.format(fold))
+            aaccuracy, uaccuracy = mean_cross_performance(history)
+            #print('Mean accuracy on fold {} \n Activity accuracy {} \n User accuracy {}'.format(fold,aaccuracy,uaccuracy))
+            f.write('Mean accuracy on fold {} \n\t Activity accuracy {} \n\t User accuracy {} \n\n'.format(fold,aaccuracy,uaccuracy))
+
+    with open(path_to_save_pre_train, 'w+') as f:
+        for fold in range(10):
+            history = np.loadtxt(fname=path_log_file+'log_history_pre_train_{}.txt'.format(fold))
             aaccuracy, uaccuracy = mean_cross_performance(history)
             #print('Mean accuracy on fold {} \n Activity accuracy {} \n User accuracy {}'.format(fold,aaccuracy,uaccuracy))
             f.write('Mean accuracy on fold {} \n\t Activity accuracy {} \n\t User accuracy {} \n\n'.format(fold,aaccuracy,uaccuracy))
