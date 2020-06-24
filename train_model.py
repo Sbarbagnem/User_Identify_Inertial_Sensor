41c532f68aac590cec6b5bdac3db9b50d7ada292
unknown <s.ventura6@campus.unimib.it>
2020-06-25 01:04:43 +0200

    fix minor

--- train_model.py
+++ train_model.py
@@ -18,29 +18,26 @@ if __name__ == '__main__':
 
     # 10-cross validation
     for model_type in ['resnet18']:
-        for dataset_name in ['unimib']:
-            for multitask in [False]:
-                for overlap in [5.0]:
-                    for magnitude in [True]:
+        for dataset_name in ['unimib', 'sbhar']:
+            for multitask in [True, False]:
+                for overlap in [5.0, 6.0, 7.0, 8.0, 9.0]:
+                    for magnitude in [True, False]:
                         if magnitude:
                             outer_dir = 'OuterPartition_magnitude_'
                             save_dir = 'log_magnitude'
                         else:
                             outer_dir = 'OuterPartition_'
                             save_dir = 'log_no_magnitude'
-                        save_dir = 'log_prova_overlap_giusto'
                         for fold in [0]:
                             print(
                                 f"Train on dataset {dataset_name}, with task {'multi_task' if multitask else 'single_task'}, on overlap {overlap}, on fold {fold}")
                             model = Model(dataset_name=dataset_name, configuration_file=configuration, multi_task=multitask, lr='dynamic',
                                         model_type=model_type, fold=fold, save_dir=save_dir, 
-                                        outer_dir=outer_dir+str(overlap)+'/', overlap=overlap, magnitude=magnitude, log=True)
+                                        outer_dir=outer_dir+str(overlap)+'/', overlap=overlap, magnitude=magnitude, log=False)
                             model.create_dataset()
                             model.load_data(augmented=False)
                             model.build_model()
                             model.print_model_summary()
                             model.loss_opt_metric()
                             model.train()
-                            # TODO 
-                                # model.plot_cm_validation(self.cm)
 
