8c6dda9bdcb4c1456d7c72d4c7bf524af69380e3
unknown <s.ventura6@campus.unimib.it>
2020-09-03 18:22:59 +0200

    fix minor

--- train_model_classification.py
+++ train_model_classification.py
@@ -129,7 +129,7 @@ if __name__ == '__main__':
             'resnet18_2D',
             'resnet18_lstm_parallel',
             'resnet18_lstm_consecutive'],
-        default='resnet18_lstm_parallel',
+        default='resnet18_2D',
         help='define model to train')
     parser.add_argument('-init_lr', '--init_lr', type=float,
                         default=0.001, help='init learning rate')
