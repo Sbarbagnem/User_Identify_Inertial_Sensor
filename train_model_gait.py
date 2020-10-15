7ad6ab2ad435f10e40020572ff5bc225f2c3c420
unknown <s.ventura6@campus.unimib.it>
2020-10-15 13:36:24 +0200

    fix minor

--- train_model_gait.py
+++ train_model_gait.py
@@ -4,6 +4,7 @@ import tensorflow as tf
 
 from model.model_gait import ModelGait
 from configuration import config
+from util.utils import str2bool
 
 if __name__ == '__main__':
 
@@ -17,7 +18,24 @@ if __name__ == '__main__':
         default='',
         help='colab path to data'
     )
-
+    parser.add_argument(
+        '-summary_model',
+        '--summary_model',
+        type=str2bool, 
+        deafult=False
+    )
+    parser.add_argument(
+        '-init_lr',
+        '--init_lr',
+        type=int, 
+        deafult=0.001
+    )
+    parser.add_argument(
+        '-log_train',
+        '--log_train',
+        type=str2bool, 
+        deafult=False
+    )    
     args = parser.parse_args()
 
     # GPU settings
@@ -31,7 +49,7 @@ if __name__ == '__main__':
     model.split_train_test()
     model.normalize_data()
     model.create_tf_dataset()
-    model.build_model(summary=True)
-    model.loss_metric(init_lr=0.1)
-    model.train_model(log=True)
+    model.build_model(summary=args.summary)
+    model.loss_metric(init_lr=args.init_lr)
+    model.train_model(log=args.log_train)
     model.test_model()
